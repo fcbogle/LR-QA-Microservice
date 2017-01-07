506a8f20409f6368672fb05eff2bcbb4d3bae86e
frankbogle <fcbogle@gmail.com>
2017-01-07 21:39:47 +0000

    Added Aspect Configuration

---
+++ src/main/java/org/frank/bogle/config/AspectConfig.java
@@ -0,0 +1,14 @@
+package org.frank.bogle.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by frankbogle on 07/01/2017.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+@Aspect
+public class AspectConfig {
+}
